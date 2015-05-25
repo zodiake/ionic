b813f008be5dc77818ce9f089714bd874ae6656a
yagamai <zodiake.wang@ihotkeys.com>
2015-05-25 14:08:54 +0800

    products category template

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic']).run(function ($ionicPlatform) {
-    $ionicPlatform.ready(function () {
+angular.module('starter', ['ionic']).run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
         if (window.cordova && window.cordova.plugins.Keyboard) {
@@ -15,62 +15,82 @@ angular.module('starter', ['ionic']).run(function ($ionicPlatform) {
         }
     });
 });
-angular.module('starter').config(function ($stateProvider, $urlRouterProvider) {
 
-    $stateProvider.state('list', {
-        url: '/list',
-        templateUrl: 'list.html'
-    }).state('category',{
-        url:'/category',
-        templateUrl:'category.html'
-    }).state('mine',{
-        url:'/mine',
-        templateUrl:'mine.html'
-    });
-});
-angular.module('starter').controller('navigator', ['$scope', '$state', function ($scope, $state) {
-    $scope.goCategory = $state.go('category');
-}]);
-angular.module('starter').controller('toController', ['$scope', '$ionicActionSheet', function ($scope, $ionicActionSheet) {
-    $scope.tasks = ['first', 'second', 'third', 'fourth'];
-    $scope.items = [
-        {
-            title: 'first',
-            description: 'description'
-        },
-        {
-            title: 'first',
-            description: 'description'
-        },
-        {
-            title: 'first',
-            description: 'description'
-        },
-        {
-            title: 'first',
-            description: 'description'
-        },
-        {
-            title: 'first',
-            description: 'description'
-        },
-    ];
-    $scope.show = function (task) {
-        var hideSheet = $ionicActionSheet.show({
-            buttons: [
-                {text: '<b>Share</b> This'},
-                {text: 'Move'}
-            ],
-            destructiveText: 'Delete',
-            titleText: 'Modify your album',
-            cancelText: 'Cancel',
-            cancel: function () {
-                // add cancel code..
-            },
-            buttonClicked: function (index) {
-                console.log(task);
-                return true;
+angular.module('starter').config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('home', {
+            url: "/home",
+            templateUrl: "../home.html",
+            controller: 'homeController'
+        }).state('tabs', {
+            abstract: true,
+            url: '/tabs',
+            templateUrl: "../tabs.html"
+        }).state('tabs.products', {
+            url: '/products',
+            views: {
+                'products-tab': {
+                    templateUrl: '../products.html',
+                    controller: 'productsController'
+                }
+            }
+        }).state('tabs.category', {
+            url: '/category',
+            views: {
+                'category-tab': {
+                    templateUrl: '../category.html',
+                    controller: 'categoryController'
+                }
             }
         });
-    };
+
+});
+
+angular.module('starter').controller('homeController', ['$timeout', '$state', function($timeout, $state) {
+    $timeout(function() {
+        $state.go('tabs.products');
+    }, 1000);
 }]);
+
+angular.module('starter').controller('productsController', ['$scope', function($scope) {
+    $scope.products = [{
+        id: '1',
+        name: 'product1'
+    }, {
+
+        id: '1',
+        name: 'product1'
+    }, {
+
+        id: '1',
+        name: 'product1'
+    }, {
+
+        id: '1',
+        name: 'product1'
+    }, {
+
+        id: '1',
+        name: 'product1'
+    }];
+
+    $scope.doRefresh = function() {
+
+    }
+}]);
+
+angular.module('starter').controller('categoryController', ['$scope', function($scope) {
+    $scope.categories = [{
+        id: '1',
+        name: 'category1'
+    }, {
+        id: '1',
+        name: 'category1'
+    }, {
+        id: '1',
+        name: 'category1'
+    }, {
+        id: '1',
+        name: 'category1'
+    }];
+}])
